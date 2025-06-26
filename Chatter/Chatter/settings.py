750a93603810a6149aed46de151b1e739a641c0f
prabeshnpl <nepalprabesh99@gmail.com>
2025-06-26 23:06:41 +0545

    Production change

--- Chatter/Chatter/settings.py
+++ Chatter/Chatter/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'chat/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # or another deploy-safe directory
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
